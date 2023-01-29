b7567afc5e32d71287d429b9eadc6a978bc35ec8
To ling <sjzbjs@outlook.com>
2023-01-29 12:56:15 +0800

    添加项目文件。

---
+++ MineMP/MineMP.cs
@@ -0,0 +1,44 @@
+﻿namespace MineMP
+{
+    internal class MineMP
+    {
+        static void RunMineShell(ref MineServer server)
+        {
+            MineShell.RunShell(ref server);
+        }
+
+        static void RunServer()
+        {
+            Console.WriteLine("[Info]Starting Server");
+
+            var mineServer = new MineServer();
+            mineServer.Init();
+            mineServer.Start();
+
+            Console.WriteLine("[Info]Server Started");
+
+            Console.WriteLine("[Info]Starting Shell");
+
+            RunMineShell(ref mineServer);
+        }
+
+        static void Main(string[] args)
+        {
+            // Welcome
+            Console.WriteLine("Hello, MineMP!\r\n");
+            Console.WriteLine("[Press Any Key to Continue]\r\n");
+
+            Console.ReadKey();
+            
+            // Continued
+
+            // Start Server
+            RunServer();
+            //
+
+
+            // We Finished
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ MineMP/MineMP/MineShell.cs
@@ -0,0 +1,17 @@
+﻿namespace MineMP
+{
+    internal static class MineShell
+    {
+        public static void RunShell(ref MineServer server)
+        {
+            Console.WriteLine("Welcome To Mine Shell!");
+            Console.WriteLine("Type \"help\" for more information.");
+
+            for (; ; )
+            {
+                Console.Write("Shell>");
+                Console.ReadLine();
+            }
+        }
+    }
+}
